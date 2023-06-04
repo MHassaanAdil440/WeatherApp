5c4e4527aad14a5b43b14821ea82071727bee0dd
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-06-04 13:59:47 +0500

    Login button added

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { View, StyleSheet, Text, Image, Dimensions, TextInput } from "react-native";
+import { View, StyleSheet, Text, Image, Dimensions, TextInput, TouchableOpacity } from "react-native";
 
 const screenWidth = Dimensions.get("window").width;
 
@@ -14,8 +14,15 @@ const Login = () => {
       </View>
       <View style={styles.bottomView}>
         <Text style={styles.heading}>Login</Text>
-        <TextInput placeholder="Email" style={styles.textinput}/>
-        <TextInput placeholder="Password" style={styles.textinput}/>
+        <TextInput placeholder="Email" style={styles.textinput} />
+        <TextInput placeholder="Password" style={styles.textinput} />
+        <View style={styles.rememberMeContainer}>
+          <TouchableOpacity style={styles.checkbox} />
+          <Text style={styles.rememberMeText}>Remember Me</Text>
+        </View>
+        <TouchableOpacity style={styles.loginButton}>
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -31,38 +38,62 @@ const styles = StyleSheet.create({
   },
   topView: {
     flex: 0.3,
-    // backgroundColor: "white",
     backgroundColor: "#F8F1F1",
     width: screenWidth,
   },
   bottomView: {
     flex: 0.7,
-    // backgroundColor: "#F8F1F1",
     width: screenWidth,
-    // alignSelf: "center",
     alignItems: "center",
     justifyContent: "center",
   },
-  //Main image style
   image: {
     width: 200,
     height: 200,
     marginTop: 50,
     alignSelf: "center",
   },
-  textinput:{
+  textinput: {
     borderWidth: 1,
     padding: 8,
     margin: 10,
-    borderRadius:25,
+    borderRadius: 25,
     borderColor: "#54B435",
     width: screenWidth * 0.7,
   },
-  heading:{
+  heading: {
     fontSize: 30,
     fontWeight: "bold",
     color: "#54B435",
-  }
+  },
+  rememberMeContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  checkbox: {
+    width: 20,
+    height: 20,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#54B435",
+    marginRight: 10,
+  },
+  rememberMeText: {
+    fontSize: 16,
+  },
+  loginButton: {
+    backgroundColor: "#54B435",
+    padding: 10,
+    borderRadius: 25,
+    width: screenWidth * 0.7,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  loginButtonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
 });
 
 export default Login;
