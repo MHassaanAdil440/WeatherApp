51c23ea8ba852199a85672f4e3c3a4da4f8f5834
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-06-09 04:48:48 +0500

    Revert "commit i am going to revert"
    This reverts commit aaeac272a86043bc525411d2658b7188a83579e1.

--- Screens/Mainscreen.js
+++ Screens/Mainscreen.js
@@ -1,39 +1,22 @@
-import React from "react";
-import {
-  View,
-  StyleSheet,
-  Text,
-  Image,
-  Dimensions,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
-import Notepad from "./Notepad";
-import Search from "./Search";
-import Settings from "./Settings";
+import React from 'react';
+import { View, StyleSheet, Text, Image, Dimensions, TextInput, TouchableOpacity } from 'react-native';
 
-const MainScreen = () => {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        {/* <Drawer.Screen name="Main" component={MainScreen} /> */}
-        <Drawer.Screen name="Search" component={Search} />
-        <Drawer.Screen name="Notepad" component={Notepad} />
-        <Drawer.Screen name="Settings" component={Settings} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
-};
+const MainApp = () => {
+    return(
+        <View style={styles.container}>
+            <Text> Main.js </Text>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff",
-  },
-});
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: "center",
+        backgroundColor: '#fff',
+    }}
+    
+    );
 
-export default MainScreen;
+export default MainApp;
